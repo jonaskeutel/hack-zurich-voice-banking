20ef8bc0d1a50e5abe7e5a1370bfae4ef275ce49
Jonas Keutel <jonas@keutel.de>
2017-09-16 05:14:03 +0200

    incrememt port by one for ws on heroku

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ var WebSocketServer = require("ws").Server
 var http = require("http")
 // var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+var port = process.env.PORT + 1 || 5000
 
 app.use(express.static(__dirname + "/"))
 
