1cc518537e522e4ba7a5c69eda97275c6511bfb8
Jonas Keutel <jonas@keutel.de>
2017-09-16 04:57:26 +0200

    reenable calling websocket from outside

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,8 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
-wss.on("connection", function(ws) {
+wss.on("connection", function(websocket) {
+    ws = websocket;
   var id = setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {  })
   }, 1000)
