b709f9177799c9e105e6ac9ffdeb4e1abe550214
Jonas Keutel <jonas@keutel.de>
2017-09-16 12:24:41 +0200

    show only one page and change according to ws requests

--- public/javascripts/ws.js
+++ public/javascripts/ws.js
@@ -1,16 +1,20 @@
 // var ws = new WebSocket("localhost:8080");
 var host = location.origin.replace(/^http/, 'ws');
-console.log(host);
-// host = host.replace(/\d+/, "5000");
-// if (host.search(/\d/) === -1) {
-//     host += ":5000";
-// }
-var websocket = new WebSocket(host);
-// var websocket = new WebSocket("ws://hack-zurich-voice-banking.herokuapp.com:8080/");
 
+var websocket = new WebSocket(host);
 
 websocket.onmessage = function(evt) {
     // $('#messages').append($('<li>').html(evt.data));
     var content = document.getElementById('alexa');
-    content.innerHTML = evt.data;
+    try {
+        var data = JSON.parse(evt.data);
+        $('.page').hide();
+        if (data.id) {
+            $('#' + data.id).show();
+        } else {
+            content.innerHTML = evt.data;
+        }
+    } catch (e) {
+        content.innerHTML = evt.data;
+    }
 };
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,16 @@ var global = require('../public/javascripts/global.js');
 
 /* GET home page. */
 router.get('/ws', function(req, res, next) {
-    global.ws.send('called from outside');
+    var id = req.query.id;
+
+    global.ws.send(JSON.stringify({id: id}));
     res.send("OK");
 });
+
+// router.get('/ws', function(req, res, next) {
+//     global.ws.send('called from outside');
+//     res.send("OK");
+// });
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
