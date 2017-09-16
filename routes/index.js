f2a4bccf20e447c0c190f3350c5a5c3f7437e905
Jonas Keutel <jonas@keutel.de>
2017-09-16 10:54:03 +0200

    send response when you hit the ws api

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var global = require('../public/javascripts/global.js');
 
 /* GET home page. */
 router.get('/ws', function(req, res, next) {
-    global.ws.send('called from outside')
+    global.ws.send('called from outside');
+    res.send("OK");
 });
 
 router.get('/', function(req, res, next) {
@@ -12,46 +13,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-
-
-// var WebSocketServer = require("ws").Server
-// var http = require("http")
-// // var express = require("express")
-// var app = express()
-// var port = parseInt(process.env.PORT) + 1 || 5000
-//
-// app.use(express.static(__dirname + "/"))
-//
-// var server = http.createServer(app)
-// server.listen(port)
-//
-// console.log("http server listening on %d", port)
-//
-// var wss = new WebSocketServer({server: server})
-// console.log("websocket server created")
-//
-// wss.on("connection", function(websocket) {
-//     ws = websocket;
-//   var id = setInterval(function() {
-//     ws.send(JSON.stringify(new Date()), function() {  })
-//   }, 1000)
-//
-//   console.log("websocket connection open")
-//
-//   ws.on("close", function() {
-//     console.log("websocket connection close")
-//     clearInterval(id)
-//   })
-// })
-// const WebSocket = require('ws');
-//
-// const wss = new WebSocket.Server({ port: 8080 });
-//
-// wss.on('connection', function connection(websocket) {
-//     ws = websocket;
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//   });
-//
-//   ws.send('something');
-// });
