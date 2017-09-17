678c3f48397865a5b56b8ab8df2a8c7f7dbc47c2
Jonas Keutel <jonas@keutel.de>
2017-09-17 07:27:26 +0200

    latest changes

--- public/javascripts/ws.js
+++ public/javascripts/ws.js
@@ -12,16 +12,17 @@ websocket.onmessage = function(evt) {
                 var balance = -60; //parseInt($('.albanaBalance')[0].innerHTML);
                 balance += (42.5 * ((100-percentage) / 100));
                 balance = balance.toFixed(2);
-                $('.albanaBalance')[0].innerHTML = balance + " €";
-                $('.michaelBalance')[0].innerHTML = "-20 €";
+                $('.albanaBalance')[1].innerHTML = balance + " €";
+                $('.michaelBalance')[1].innerHTML = "-20,00 €";
                 // $('.friend-balance').prepend("You pay " + percentage + " %, Albana has to pay " + (100 - percentage) + " %.");
                 $('#ratio')[0].innerHTML = "You pay " + percentage + " %, Albana has to pay " + (100 - percentage) + " %.";
                 $('#' + data.id).show();
                 break;
+            case 'transaction':
             case 'transactions':
-                $('#transaction2')[0].innerHTML = $('.albanaBalance')[0].innerHTML;
                 // $('#michael span')[2].innerHTML = $('#michael span')[1].innerHTML;
                 $('#' + data.id).show();
+                $('#transaction2')[0].innerHTML = ($('.albanaBalance')[1].innerHTML).substring(1);
                 break;
             default:
                 $('#' + data.id).show();
@@ -31,4 +32,4 @@ websocket.onmessage = function(evt) {
     }
 };
 
-setTimeout(function(){$('#login').show();}, 500)
+setTimeout(function(){$('#login').show();}, 800)
